f11e102088e7ba96fb4d326671c457307ae312db
Ibrahim Olanrewaju <semslam007@gmail.com>
2022-03-13 05:16:38 +0100

    add constant value

--- api/humanMoves/humanMoves.controllers.js
+++ api/humanMoves/humanMoves.controllers.js
@@ -4,9 +4,20 @@ const {successResponse,errorResponse} = require("../../response/apiResponse");
 const {HttpCodes} = require("../../libraries/sustainedValues");
 const {isObjEmpty} = require("../../libraries/Validator");
 const auth = require("../../middleware/jsonwebtokenAuthentication");
-
+const {repeatedValues} = require("../../libraries/sustainedValues")
+const [ROCK,PAPER,SCISSORS,COMPUTER,TIED,HUMAN_VS_COMPUTER,COMPUTER_VS_COMPUTER] = repeatedValues;
 
 const rockPaperScissor = new RockPaperScissor("api");
+
+// wrongRequest = (res,message)=>{
+//     // check if the gameOptionsAndMoves is empty
+//     if(isObjEmpty(gameOptionsAndMoves))
+//         return errorResponse(res,HttpCodes.NOTACCEPTABLE,{ message:"Sorry, you have to start a new game" });
+    
+//    else if(gameOptionsAndMoves.playingMode === COMPUTER_VS_COMPUTER)
+//        return errorResponse(res,HttpCodes.NOTACCEPTABLE,{ message:`You are not meant to provide ${message}; the game is played by computer versus computer` });
+    
+// }
 
 let gameOptionsAndMoves = {};
 
@@ -19,7 +30,7 @@ gameOptionsAndMoves.playingMode = req.body.playersType;
 
 // if computer vs computer no need to provide a name and moves
 let response = {message:`You have chosen ${gameOptionsAndMoves.playingMode} as playerTypes, You are playing ${req.body.gameRound} round`};
-    if(req.body.playersType === "humanVsComputer"){
+    if(req.body.playersType === HUMAN_VS_COMPUTER){
         rockPaperScissor.gameStatus = false;
         successResponse(res,HttpCodes.OK,"Please provide the player name",response);
     }else{
@@ -32,30 +43,38 @@ let response = {message:`You have chosen ${gameOptionsAndMoves.playingMode} as p
 const providePlayerName = (req, res)=>{
     // check if the gameOptionsAndMoves is empty
     if(isObjEmpty(gameOptionsAndMoves)){
-        return errorResponse(res,HttpCodes.NOTACCEPTABLE,{ message:"Sorry, you have to restart the game" });
+        return errorResponse(res,HttpCodes.NOTACCEPTABLE,{ message:"Sorry, you have to start a new game" });
     }
-    if(gameOptionsAndMoves.playingMode === "computerVsComputer"){
-       return errorResponse(res,HttpCodes.NOTACCEPTABLE,{ message:"You are not meant to provide player name, the game is playing by computer vs computer" });
+    if(gameOptionsAndMoves.playingMode === COMPUTER_VS_COMPUTER){
+       return errorResponse(res,HttpCodes.NOTACCEPTABLE,{ message:"You are not meant to provide a player name; the game is played by computer versus computer" });
     }else{
         gameOptionsAndMoves.playerName = req.body.playerName
         let response = {message:`Your player name is: ${req.body.playerName}`,nextRequest:['Please choose your move',["rock", "paper", "scissors"]]};
         successResponse(res,HttpCodes.OK,"Please choose your move",response);
     }
+
+    
+    // wrongRequest(res,"a player name");
+    // gameOptionsAndMoves.playerName = req.body.playerName
+    //     let response = {message:`Your player name is: ${req.body.playerName}`,nextRequest:['Please choose your move',["rock", "paper", "scissors"]]};
+    //     successResponse(res,HttpCodes.OK,"Please choose your move",response);
    
 }
 
 const gameMove = (req, res)=>{
     // check if the gameOptionsAndMoves is empty
     if(isObjEmpty(gameOptionsAndMoves)){
-        return errorResponse(res,HttpCodes.NOTACCEPTABLE,{ message:"Sorry, you have to restart the game" });
+        return errorResponse(res,HttpCodes.NOTACCEPTABLE,{ message:"Sorry, you have to start a new game" });
     }
-    if(gameOptionsAndMoves.playingMode === "computerVsComputer"){
-        return errorResponse(res,HttpCodes.NOTACCEPTABLE,{ message:"You are not meant to provide player name, the game is playing by computer vs computer" });
+    if(gameOptionsAndMoves.playingMode === COMPUTER_VS_COMPUTER){
+        return errorResponse(res,HttpCodes.NOTACCEPTABLE,{ message:"You are not meant to provide any move; the game is played by computer vs. computer" });
     }else{
         gameOptionsAndMoves.externalMove = req.body.playersMove;
         rockPaperScissor.apiPlayingProcess(res,gameOptionsAndMoves);
     }
-   
+    // wrongRequest(res,"any move");
+    // gameOptionsAndMoves.externalMove = req.body.playersMove;
+    // rockPaperScissor.apiPlayingProcess(res,gameOptionsAndMoves);
 }
 
 
