b5160b1cd874e4bdde44a4dfd21150113ae39dbc
Ibrahim Olanrewaju <semslam007@gmail.com>
2022-03-15 02:09:37 +0100

    clean up test server

--- test/createServer.js
+++ test/createServer.js
@@ -1,16 +1,10 @@
 const express = require("express");
 const routes = require("../api/routes");
-const {authenticateToken} = require("../middleware/jsonwebtokenAuthentication");
 
 function createServer() {
   const app = express();
-
   app.use(express.json());
-
-//   app.use(authenticateToken);
-
   routes(app);
-
   return app;
 }
 
